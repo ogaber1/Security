b5b92c3040a7427a46a3b7a5e3b9ff757e3fffb0
Mohamed Hefny <113836440+hefnyy@users.noreply.github.com>
2025-10-27 01:08:49 +0300

    Add Inverse Permutation

---
+++ inverse_permutation.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+#ifndef INVERSE_PERMUTATION_H
+#define INVERSE_PERMUTATION_H
+
+#include <stdint.h>
+uint64_t inverse_permutation(uint64_t block);
+
+#endif //INVERSE_PERMUTATION_H
